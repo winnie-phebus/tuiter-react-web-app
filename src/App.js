9660e5b2aac1baea3b68b1026cd72e4b9eeb803a
Winnie Phebus <winn.phebus@gmail.com>
2023-03-02 16:49:03 -0500

    went to OH

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <div className="container">
         <Routes>
-          <Route index path="" element={<Labs />} />
+          <Route index element={<Labs />} />
           <Route path="/hello" element={<HelloWorld />} />
           <Route path="/tuiter" element={<Tuiter />} />
         </Routes>
--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import PostSummaryList from "../post-summary-list";
 import "./index.css";
+
 const ExploreComponent = () => {
   return (
-    <>
+    <div>
       <div className="row">
         <div className="col-11 position-relative">
           <input
@@ -34,13 +37,13 @@ const ExploreComponent = () => {
         </li>
       </ul>
       <div className="position-relative mb-2">
-        <img src="/images/starship.jpg" className="w-100" />
+        <img src="/images/starship.jpeg" className="w-100" />
         <h1 className="position-absolute wd-nudge-up text-white">
           SpaceX Starship
         </h1>
       </div>
       <PostSummaryList />
-    </>
+    </div>
   );
 };
 export default ExploreComponent;
